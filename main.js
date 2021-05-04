75403b039cc3cbe7a9067d037c0fbeae4ca04f6d
drdonajose <78793626+drdonajose@users.noreply.github.com>
2021-05-04 18:07:43 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,14 @@
+console.log("My first console project");
+var first_name="Dona";
+var second_name="jose";
+var full_name=first_name.concat(second_name);
+var a=10;
+var b=15;
+var add= a+b;
+console.log(add);
+var sub= a-b;
+console.log(sub);
+var mul=a*b;
+console.log(mul);
+var div=a/b;
+console.log(div);
